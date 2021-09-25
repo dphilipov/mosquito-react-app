ce19c39522262c95dd89cd9f5c3de49905b195dc
Dimitar Filipov <grinndev@protonmail.com>
2021-09-25 16:31:33 +0300

    update Notifications styling

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,10 +1,18 @@
 import style from './Notification.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
-const Notification = ({type, message}) => {
+const Notification = ({ type, message }) => {
 
-    return(
+    return (
         <div className={`${style.notification} ${type === 'good' ? style.good : style.bad}`}>
-            {message}
+            {type === 'good' ?
+                <FontAwesomeIcon icon={faCheckCircle} className={style.notificationIcon} />
+                :
+                <FontAwesomeIcon icon={faTimesCircle} className={style.notificationIcon}/>
+            }
+            <span className={style.notificationMessage}>{message}</span>
         </div>
     )
 }

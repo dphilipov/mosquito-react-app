c595f83fdb30b19c36539a5e69aec9e2559dc6ee
Dimitar Filipov <grinndev@protonmail.com>
2021-10-04 13:59:00 +0300

    change spinner & add spinner to Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { UserConsumer } from '../userContext';
 import firebase from '../../config/firebase.js';
+import spinner from './ajax-loader.gif';
 import Article from '../Article/Article';
 import Comment from '../Comment/Comment';
 import authServices from '../../services/authServices';
@@ -10,9 +11,11 @@ import postServices from '../../services/postServices';
 
 
 const Profile = (props) => {
-    let [activities, setActivities] = useState([]);
-    let [comments, setComments] = useState([]);
-    let [updateParent, setUpdateParent] = useState(false);
+    const [activities, setActivities] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [updateParent, setUpdateParent] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
 
     let history = useHistory();
 
@@ -61,12 +64,14 @@ const Profile = (props) => {
 
     // RE-FETCH ACTIVITY AFTER LIKING/DISLIKING
     useEffect(() => {
+        setIsLoading(true);
         let user = authServices.getUserData('user')
 
         postServices.getProfileActivity(user.uid, 5)
             .then(activitiesData => {
                 setActivities(activitiesData);
                 setUpdateParent(false);
+                setIsLoading(false);
             })
             .catch(err => console.log(err))
     }, [updateParent])
@@ -88,26 +93,33 @@ const Profile = (props) => {
                                     <li onClick={showProfileActivityHandler}>View {userCheck.email}`s Latest Activity &#9660;</li>
                                     <li onClick={showProfileCommentsHandler}>View {userCheck.email}`s Latest Comments &#9660;</li>
                                 </ul>
-                                {activities.length > 0
-                                    ? activities.map(activity =>
-                                        <Article
-                                            key={activity.id}
-                                            articleData={activity}
-                                            updateParent={updateParentHandler}
-                                        />
-                                    )
-                                    : ''
-                                }
 
-                                {comments.length > 0
-                                    ? comments.map((comment, index) =>
-                                        <Comment
-                                            key={index}
-                                            commentInfo={comment}
-                                            updateParent={updateParentHandler}
-                                        />
-                                    )
-                                    : ''
+                                {isLoading === true ?
+                                    <img className={style.loader} src={spinner} alt="loader"></img>
+                                    :
+                                    <>
+                                        {activities.length > 0
+                                            ? activities.map(activity =>
+                                                <Article
+                                                    key={activity.id}
+                                                    articleData={activity}
+                                                    updateParent={updateParentHandler}
+                                                />
+                                            )
+                                            : ''
+                                        }
+
+                                        {comments.length > 0
+                                            ? comments.map((comment, index) =>
+                                                <Comment
+                                                    key={index}
+                                                    commentInfo={comment}
+                                                    updateParent={updateParentHandler}
+                                                />
+                                            )
+                                            : ''
+                                        }
+                                    </>
                                 }
                             </div>
                         </div>
